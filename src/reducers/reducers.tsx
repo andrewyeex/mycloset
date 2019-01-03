3319b89f65c9e89e68f5f0055affd5daf7985da4
andrewyeex <andrew.yee@mail.com>
2019-01-03 07:46:52 -0800

    comment out redux files

--- src/reducers/reducers.tsx
+++ src/reducers/reducers.tsx
@@ -1,32 +1,33 @@
-import { closetActions } from '../actions/actions'
-import {
-  REQUEST_TO_CREATE_NEW_CLOTHINGS,
-  REQUEST_TO_CREATE_CLOTHINGS_SUCCESS,
-  REQUEST_TO_CREATE_CLOTHINGS_FAIL
-} from '../actionTypes/actionTypes'
+// import {
+//   REQUEST_TO_CREATE_CLOTHINGS_SUCCESS,
+//   REQUEST_TO_CREATE_CLOTHINGS_FAIL,
+//   REQUEST_TO_CREATE_NEW_CLOTHINGS
+// } from '../actionTypes/actionTypes'
+// import { closetActions } from '../actions/actions'
 
-export interface StoreState {
-  data: object
-}
 
-export function closet(state: StoreState, action: closetActions): StoreState {
-  switch(action.type){
-    case REQUEST_TO_CREATE_NEW_CLOTHINGS:
-      return {
-        ...state,
-        data: {}
-      }
-    case REQUEST_TO_CREATE_CLOTHINGS_SUCCESS:
-      return {
-        ...state,
-        data: action.data
-      }
-    case REQUEST_TO_CREATE_CLOTHINGS_FAIL:
-      return {
-        ...state,
-        data: {}
-      }
+// export interface StoreState {
+//   data: object
+// }
 
-  }
-  return state;
-}
+// export function closet(state: StoreState, action: closetActions): StoreState {
+//   switch(action.type){
+//     case REQUEST_TO_CREATE_NEW_CLOTHINGS:
+//       return {
+//         ...state,
+//         data: {}
+//       }
+//     case REQUEST_TO_CREATE_CLOTHINGS_SUCCESS:
+//       return {
+//         ...state,
+//         data: action.data
+//       }
+//     case REQUEST_TO_CREATE_CLOTHINGS_FAIL:
+//       return {
+//         ...state,
+//         data: {}
+//       }
+
+//   }
+//   return state;
+// }
